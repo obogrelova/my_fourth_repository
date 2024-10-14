278558d1495c728919c3bd777faf10247b17b826
obogrelova <on.obogrelova@gmail.com>
2024-10-14 20:15:39 +0500

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Игра Тир")
-icon = pygame.image.load("img\icon.jpg")
+icon = pygame.image.load("img/icon.jpg")
 pygame.display.set_icon(icon)
 
-target_img = pygame.image.load("img\target.png")
+target_img = pygame.image.load("img/target.png")
 target_width = 80
 target_height = 80
 
